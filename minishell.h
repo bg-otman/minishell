b0693ace24cbeefd3b6c2bcb36f6b1de943515be
Othmane Bouizi <obouizi@c4r10p3.1337.ma>
2025-04-05 11:03:28 +0000

    little changes

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: obouizi <obouizi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/01 18:48:13 by asajed            #+#    #+#             */
-/*   Updated: 2025/04/03 14:12:45 by obouizi          ###   ########.fr       */
+/*   Updated: 2025/04/05 10:04:48 by obouizi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,23 +26,19 @@
 # include <fcntl.h>
 # include <errno.h>
 # include <dirent.h>
-# include "parser/parser.h"
+// # include "parser/parser.h"
 
 # define true 1
 # define false 0
 # define PROMPT "\x1B[32mminishell ~ \e[1m"
-# define O_APPEND: 1024
-# define O_TRUNC: 512
 # define INPUT_FILE 0
 # define OUTPUT_FILE 1
 
 typedef enum e_types
 {
     COMMAND,
-    ARGUMENT,
     PIPE,
     REDIRECTION,
-    FILENAME,
     LOGICAL_OP,
     PARENTHESIS,
 } e_types;
@@ -52,25 +48,23 @@ typedef struct  s_redirection
 {
 	int    type; // input or output
 	int    open_mode;
-	char   *filename;
+	char   *file_name;
 	struct s_redirection *next; // if a command has more than one redirection
 } t_redir;
 
 typedef struct s_shell
 {
-	char		**args;
-	char		*cmd;
-	t_redir		*redirections;
-	e_types		cmd_type;
+	char			**args;
+	char			*cmd;
+	t_redir			*redirections;
+	struct s_shell	*next;
+	e_types			cmd_type;
 }       t_shell;
 
 // PARSER
 typedef struct s_tree
 {
-	// e_types         type;
-	// char            *cmd;
-	// char            **args;
-	t_shell *node;
+	t_shell			*node;
 	struct s_tree   *left;
 	struct s_tree   *right;
 } t_tree;
@@ -78,5 +72,8 @@ typedef struct s_tree
 // void    lexer(char *line, t_shell *shell);
 int		fdprintf(int fd, const char *str, ...);
 
+// temperory funs
+t_shell	*get_commands();
+// void	display_tree(tree);
 
 #endif
