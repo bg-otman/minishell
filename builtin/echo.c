98c7771fe2bd825cd1d80b2fdb37e4b5683c39ca
Aymanesaj <sajidaymane@gmail.com>
2025-04-15 09:32:10 +0100

    add_builtins

---
+++ builtin/echo.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asajed <asajed@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/13 10:58:54 by asajed            #+#    #+#             */
+/*   Updated: 2025/04/15 09:29:49 by asajed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int	options(char **args, int *i)
+{
+	int	j;
+	int	nl;
+
+	if (!ft_strcmp(args[0], "--"))
+		return (1);
+	j = 0;
+	nl = 1;
+	while (args[(*i)])
+	{
+		if (args[(*i)][0] != '-')
+			return (nl);
+		else
+		{
+			j = 1;
+			while (args[(*i)][j] && args[(*i)][j] == 'n')
+				j++;
+			if (args[(*i)][j] || (!args[(*i)][j] && args[(*i)][j - 1] == '-'))
+				return (nl);
+			else
+				nl = 0;
+		}
+		(*i)++;
+	}
+	return (nl);
+}
+
+int	echo(char **args)
+{
+	int	i;
+	int	nl;
+
+	if (!args)
+		return (printf("\n"), 0);
+	i = 0;
+	nl = options(args, &i);
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		i++;
+		if (args[i])
+			printf(" ");
+	}
+	if (nl)
+		printf("\n");
+	return (0);
+}
